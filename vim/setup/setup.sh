0651560c3bef7021113df600f02b354ac4125cbf
Ryan Carney <arecarn@gmail.com>
2014-03-14 22:58:34 -0700

    Dun Dun Dun!

--- vim/setup/setup.sh
+++
@@ -1,5 +0,0 @@
-# install Neobundle to manage plugins
-git clone https://github.com/Shougo/neobundle.vim.git bundle/neobundle.vim
-
-# start vim loading the vimrc file then close right after
-vim -c 'q'

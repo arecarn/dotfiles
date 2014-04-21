7a06baa0b7d3378deeb0b28ecca0fd519656f22f
Ryan Carney <arecarn@gmail.com>
2014-04-21 07:57:48 -0700

    Finished making dotfiles actually have dots

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 
 dir=~/dotfiles                             # dotfiles directory
 olddir=~/dotfiles_old                      # old dotfiles backup directory
-dotfiles=".inputrc .tmux.conf .mutt .gitconfig .zshrc .oh-my-zsh .vim .ctags" # list of files/folders to symlink in homedir
+dotfiles=.inputrc .tmux.conf .mutt .gitconfig .zshrc .oh-my-zsh .vim .ctags" # list of files/folders to symlink in homedir
 
 ############################
 # Code

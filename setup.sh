48ea8506a173607ae6b7d0ff3c1b6f6fc4ea45ac
Ryan Carney <arecarn@gmail.com>
2014-06-18 09:40:36 -0700

    setup: Added a script that might be useful later

--- setup.sh
+++ setup.sh
@@ -24,14 +24,6 @@ echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-echo "Moving any existing dotfiles from ~ to $olddir"
-for file in $dotfiles; do
-    mv ~/$file $olddir/$file
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
-done
-echo "...done"
 function grab 
 {
     repo=$1
@@ -47,12 +39,39 @@ function grab
     fi
 }
 
-grab https://github.com/VitaliyRodnenko/geeknote.git          "~/dotfiles/boobz"
-grab https//:github.com/lpenz/atdtool.git                     "mybin/installs/atdtool"
-grab https//:github.com/robbyrussell/oh-my-zsh.git            ".oh-my-zsh"
-grab https//:github.com/s7anley/zsh-geeknote.git              "~/dotfiles/oh-my-zsh/custom/plugins/geeknote"
-grab https://github.com/Shougo/neobundle.vim.git              ".vim/bundle/neobundle.vim"
-grab https://github.com/altercation/mutt-colors-solarized.git "mutt/colors/mutt"
+# this was pulled from a reddit from user
+# http://www.reddit.com/r/vim/comments/28e8z5/simple_vim_plugin_downloader/ciaer2x
+# it might be useful
+
+function lookatthis
+{
+    cnt=0
+    cd ~/.vim/bundle &&
+        for d in *; do
+            if [ -d $d/.git ]; then
+                echo "Updating $d"
+                cd $d && git pull &
+                # Prevent having too many subprocesses
+                (( (cnt += 1) % 16 == 0 )) && wait
+            fi
+        done
+        wait
+}
+
+grab https://github.com/lpenz/atdtool.git                     "$HOME/dotfiles/mybin/installs/atdtool"
+grab https://github.com/robbyrussell/oh-my-zsh.git            "$HOME/dotfiles/.oh-my-zsh"
+grab https://github.com/s7anley/zsh-geeknote.git              "$HOME/dotfiles/oh-my-zsh/custom/plugins/geeknote"
+grab https://github.com/Shougo/neobundle.vim.git              "$HOME/dotfiles/.vim/bundle/neobundle.vim"
+grab https://github.com/altercation/mutt-colors-solarized.git "$HOME/dotfiles/.mutt/colors/mutt"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
+for file in $dotfiles; do
+    mv ~/$file $olddir/$file
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
+echo "...done"
 
 
 # run Vim setup

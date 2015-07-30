59773274ad87aa10ca8379199c9b60d818c27070
Ryan Carney <arecarn@gmail.com>
2015-07-29 22:01:58 -0700

    setup: add symlink aliases to vimfiles & HOME/.vimrc
    Add symlink aliases:
    - from .vim -> vimfiles  for windows compatability
    - from .vim/vimrc -> HOME/vimrce for compatibility with older version of
      vim
    
    Also:
    cleanup script by moving variable declaration to the top of the file

--- setup.py
+++ setup.py
@@ -1,36 +1,19 @@
 from os.path import join
 from os.path import expanduser
-from os import mkdir
-from os import rename
 from os import name
 import os
 import datetime
 
-if os.name == "nt":
-    isWindows = True
-    def symlink_ms(source, link_name):
-        import ctypes
-        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
-        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
-        csl.restype = ctypes.c_ubyte
-        flags = 1 if os.path.isdir(source) else 0
-        try:
-            if csl(link_name, source.replace('/', '\\'), flags) == 0:
-                raise ctypes.WinError()
-        except:
-            pass
-    os.symlink = symlink_ms
+# ============================================================================
+HOME = expanduser("~")
+dotfiles_dir = join(HOME, "dotfiles")
 
-
-def timeStamped(fname, fmt='{fname}_%Y-%m-%d-%H-%M-%S'):
+def time_stamped(fname, fmt="{fname}_%Y-%m-%d-%H-%M-%S"):
     return datetime.datetime.now().strftime(fmt).format(fname=fname)
 
-home = expanduser("~")
-dotfiles_dir = join(home, 'dotfiles')
-dotfiles_old_dir = join(home, timeStamped('dotfiles_old'))
+dotfiles_old_dir = join(HOME, time_stamped("dotfiles_old"))
 
-
-dotfiles = [
+DOTFILES = [
     ".inputrc",
     ".tmux.conf",
     ".mutt",
@@ -42,37 +25,51 @@ dotfiles = [
     ".ctags",
 ]
 
-blank_files = [
-    join(home, ".zshrc_local"),
-    join(home, ".vim/vimrc_local"),
-    join(home, ".Trash"),
-    join(home, ".gitconfig_local"),
+BLANK_FILES = [
+    join(HOME, ".zshrc_local"),
+    join(HOME, join(".vim", "vimrc_local")),
+    join(HOME, ".Trash"),
+    join(HOME, ".gitconfig_local"),
 ]
 
+ALIASES = [
+    (join(HOME, '.vim'), join(HOME, 'vimfiles')),
+    (join(HOME, ".vim", "vimrc"), join(HOME, ".vimrc")),
+]
 
-print("Creating blank local config files")
-for blank_file in blank_files:
-    open(blank_file, "w")
+# ============================================================================
 
-print("Creating %s for backup of any existing dotfiles in %s") % (dotfiles_old_dir, home)
-mkdir(dotfiles_old_dir)
+print("Creating {0} for backup of any existing dotfiles in {1}".format(dotfiles_old_dir, HOME))
+os.mkdir(dotfiles_old_dir)
 
-
-print("Moving any existing dotfiles from %s to %s") % (dotfiles_dir, dotfiles_old_dir)
-for dotfile in dotfiles:
-    source = join(home, dotfile)
+print("Moving any existing dotfiles from {0} to {1}".format(dotfiles_dir, dotfiles_old_dir))
+for dotfile in DOTFILES:
+    source = join(HOME, dotfile)
     target = join(dotfiles_old_dir, dotfile)
     try:
-        rename(source, target)
+        os.rename(source, target)
     except:
         pass
 
-    print("Creating symlink to %s") % (dotfile)
-    if isWindows:
-        symlink_ms(source, target)
-    else:
-        os.symlink(source, target)
+    print("Symlinking new dotfiles any existing dotfiles from {1} to {0}".format(dotfiles_dir, dotfiles_old_dir))
+    source = join(dotfiles_dir, dotfile)
+    target = join(HOME, dotfile)
+    print("Creating symlink {0}".format(target))
+    os.symlink(source, target)
 
-print("All Done ")
+for blank_file in BLANK_FILES:
+    print("Creating blank local config file: {0} if needed".format(blank_file))
+    open(blank_file, "w")
 
 
+for alias in ALIASES:
+    source = alias[1]
+    target = join(dotfiles_old_dir, os.path.basename(alias[1]))
+    try:
+        os.rename(source, target)
+    except:
+        pass
+
+    os.symlink(alias[0], alias[1])
+
+print("All Done")

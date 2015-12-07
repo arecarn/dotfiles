8d6aba2c72a50cf8c3fbb8003fa2eef2e7ae011d
Ryan Carney <arecarn@gmail.com>
2015-12-06 22:32:49 -0800

    setup: move config into a separate file
    the separate file only contains variables and don't depend on any
    modules

---
+++ setup_config.py
@@ -0,0 +1,82 @@
+FILES = [
+    "~/.zshrc_local",
+    "~/.vimrc_local",
+    "~/.gitconfig_local",
+]
+
+DIRECTORIES = [
+    "~/.Trash",
+]
+
+DOTFILES = [
+        {
+            "source" : ".ctags",
+            "name" : ".ctags",
+            "location" : "~",
+        },
+
+        {
+            "source" : ".gitconfig",
+            "name" : ".gitconfig",
+            "location" : "~",
+        },
+
+        {
+            "source" : ".gitignore_global",
+            "name" : ".gitignore_global",
+            "location" : "~",
+        },
+
+        {
+            "source" : ".inputrc",
+            "name" : ".inputrc",
+            "location" : "~",
+        },
+
+        {
+            "source" : ".mutt",
+            "name" : ".mutt",
+            "location" : "~",
+        },
+
+
+        # Tmux
+        {
+            "source" : ".tmux",
+            "name" : ".tmux",
+            "location" : "~",
+        },
+        {
+            "source" : ".tmux/.tmux.conf",
+            "name" : ".tmux.conf",
+            "location" : "~",
+        },
+
+        # Vim
+        {
+            "source" : ".vim",
+            "name" : ".vim",
+            "location" : "~",
+        },
+        {
+            "source" : ".vim/vimrc",
+            "name" : ".vimrc",
+            "location" : "~",
+        },
+        {
+            "source" : '.vim',
+            "name" : "vimfiles",
+            "location" : "~",
+        },
+
+        # Z-Shell
+        {   "source" : ".zsh",
+            "name" : ".zsh",
+            "location" : "~",
+        },
+        {
+            "source" : ".zsh/.zshrc",
+            "name" : ".zshrc",
+            "location" : "~",
+        },
+]

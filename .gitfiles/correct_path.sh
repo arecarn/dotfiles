70570c679c143c7415561c03d4a23efa021fb5db
Ryan Carney <arecarn@gmail.com>
2015-12-14 21:26:18 -0800

    git: move longer alias scripts into separate files

---
+++ .gitfiles/correct_path.sh
@@ -0,0 +1,7 @@
+# This script ensures that the relative file path is used for aliases that
+# include a "!" at the start, otherwiese they are run at the root of the
+# repository.
+#
+# Note GIT_PREFIX is empty at repository root
+
+test -n "$GIT_PREFIX" && cd $GIT_PREFIX;
---
+++ .gitfiles/status_short.sh
@@ -0,0 +1,5 @@
+source ~/dotfiles/.gitfiles/correct_path.sh
+git status
+git status --short --branch
+git --no-pager stash-list-pretty
+true

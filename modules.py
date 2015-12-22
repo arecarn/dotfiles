859b257abe69d4228a29cdfc9a36c0376c095d3c
Ryan Carney <arecarn@gmail.com>
2015-12-21 18:04:37 -0800

    modular: add configuration file for modular

---
+++ modules.py
@@ -0,0 +1,41 @@
+MODULES = [
+
+        # mutt colors?
+
+        {
+            "url" : "https://github.com/junegunn/fzf",
+            "directory" : "~/.fzf/fzf",
+            "rev" : "master",
+        },
+
+        {
+            "url" : "https://github.com/tmux-plugins/tpm",
+            "directory" : "~/.tmux/plugins/tpm",
+            "rev" : "master",
+        },
+
+        {
+            "url" : "https://github.com/Shougo/neobundle.vim.git",
+            "directory" : "~/.vim/bundle/neobundle.vim",
+            "rev" : "master",
+        },
+
+        {
+            "url" : "https://github.com/tarjoilija/zgen.git",
+            "directory" : "~/.zsh/zgen",
+            "rev" : "master",
+        },
+
+        {
+            "url" : "https://github.com/arecarn/modular.git",
+            "directory" : "~/modular",
+            "rev" : "master",
+        },
+
+        {
+            "url" : "https://github.com/arecarn/dotfiles.git",
+            "directory" : "~/dotfiles",
+            "rev" : "master",
+        },
+]
+

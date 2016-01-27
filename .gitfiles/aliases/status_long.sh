68ca002a688e7192893aac2950873ef6727fc2a7
Ryan Carney <arecarn@gmail.com>
2016-01-27 15:10:46 -0800

    reorganize top level dir to remove hidden directories

--- .gitfiles/aliases/status_long.sh
+++
@@ -1,2 +0,0 @@
-source ~/dotfiles/.gitfiles/aliases/correct_path.sh
-git status "$@" &&  printf '\n'; git --no-pager stash-list-pretty --name-status
--- git/bin/status_short.sh
+++ git/bin/status_short.sh
@@ -1,2 +1,2 @@
-source ~/dotfiles/.gitfiles/aliases/correct_path.sh
+source ~/dotfiles/git/bin/correct_path.sh
 git status --short --branch "$@" && git --no-pager stash-list-pretty

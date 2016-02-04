cf975ab867987e73de8cd87f193c126b7087f1a6
Ryan Carney <arecarn@gmail.com>
2016-02-04 08:22:31 -0800

    git: short status: add note about rebase in progress

--- git/bin/status_short.sh
+++ git/bin/status_short.sh
@@ -1,2 +1,8 @@
 source ~/dotfiles/git/bin/correct_path.sh
+
+# Note if a rebase is in Progress
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+ls `git rev-parse --git-dir` |  grep "rebase" --silent && printf "${YELLOW}NOTE:${NC} Rebase In Progress\n"
+
 git status --short --branch "$@" && git --no-pager stash-list-pretty

120d30c990b2ba499aa20e59b272a3d19f49a408
Ryan Carney <arecarn@gmail.com>
2016-02-04 09:45:29 -0800

    git: rework and clean up short status script

--- git/bin/status_short.sh
+++ git/bin/status_short.sh
@@ -1,8 +1,18 @@
 source ~/dotfiles/git/bin/correct_path.sh
 
-# Note if a rebase is in Progress
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
-ls `git rev-parse --git-dir` |  grep "rebase" --silent && printf "${YELLOW}NOTE:${NC} Rebase In Progress\n"
 
-git status --short --branch "$@" && git --no-pager stash-list-pretty
+if git rev-parse; then # if we are in a git repo
+
+    # make a note if a rebase is in progress
+    if ls `git rev-parse --git-dir` | grep "rebase" --silent; then
+        printf "${YELLOW}note:${NC} rebase in progress\n"
+    fi
+
+    # print our status
+    git status --short --branch "$@"
+
+    # print our stashes
+    git --no-pager stash-list-pretty
+fi

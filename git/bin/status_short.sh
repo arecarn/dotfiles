e3ffb0f3e7f415c71665c0c266b6f799211e68f9
Ryan Carney <arecarn@gmail.com>
2016-02-08 21:13:00 -0800

    git: make rebase note more prominent for short status

--- git/bin/status_short.sh
+++ git/bin/status_short.sh
@@ -4,15 +4,10 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 if git rev-parse; then # if we are in a git repo
-
-    # make a note if a rebase is in progress
+    # notify when a rebase is in progress
     if ls `git rev-parse --git-dir` | grep "rebase" --silent; then
-        printf "${YELLOW}note:${NC} rebase in progress\n"
+        printf "${YELLOW}Rebase In Progress\n"
     fi
-
-    # print our status
     git status --short --branch "$@"
-
-    # print our stashes
     git --no-pager stash-list-pretty
 fi

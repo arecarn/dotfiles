67c869fcdc6243e7a01eb8db61984cda9838f229
Ryan Carney <arecarn@gmail.com>
2016-02-08 22:45:16 -0800

    git: add new git change-commits cmd & others

--- bin/git_execute_in_git_dir.sh
+++ bin/git_execute_in_git_dir.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+
 # Execute a command at the top level of a Git repository directory while
 # taking into account submodules
 
 GIT_TOP="${GIT_DIR%%/.git/modules/*}";
+
 [ ".$GIT_TOP" != ".$GIT_DIR" ] && cd "$GIT_TOP";
+
 exec "$@";
 

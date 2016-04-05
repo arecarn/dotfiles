84b469c6064074ddf0b95dced9e9d1627a8fe6dd
Ryan Carney <arecarn@gmail.com>
2016-04-05 13:53:28 -0700

    bin: create an example argument parser

---
+++ bin/argument_parser_example.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+other_args=''
+handle_other_argument(){
+    # save all the non-named arguments so they can be reinserted back into the
+    # positional parameters at the end of the argument parsing
+    other_args="$other_args $@"
+}
+
+# default values
+name=""
+file=""
+log=""
+default=0
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -n|--name) name="$2"; shift 1;;
+        -f|--file) file="$2"; shift 1;;
+        -l|--log) log="$2"; shift 1;;
+        -d|--default) default=1;;
+        --name|--file|--log) echo "$1 requires an argument" >&2; exit 1;;
+        -*) echo "unknown option: $1" >&2; exit 1;;
+        *) handle_other_argument "$1";
+    esac
+    shift 1
+done
+
+# reinsert non-named arguments back into positional parameters ($1 $2 ..)
+eval set -- $other_args
+
+echo name       = "$name"
+echo file       = "$file"
+echo log        = "$log"
+echo default    = "$default"
+echo other args = "$@"

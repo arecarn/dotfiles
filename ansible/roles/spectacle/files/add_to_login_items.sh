589024f1709112cf8e096cbb9e656fe1a0c7933d
Ryan Carney <arecarn@gmail.com>
2017-09-09 09:52:10 -0700

    ansible: spectacle: fix script to add spectacle to login items

--- ansible/roles/spectacle/files/add_to_login_items.sh
+++ ansible/roles/spectacle/files/add_to_login_items.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 path="$(echo /opt/homebrew-cask/Caskroom/spectacle/*/*.app)";
-eval 'osascript -e '\''tell application "System Events" to make new login item with properties { path: "'"$path"'" } at end'\'
+logged_in_user=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
+sudo su "${logged_in_user}" -c  'osascript -e '\''tell application "System Events" to make new login item with properties { path: "'"$path"'" } at end'\'

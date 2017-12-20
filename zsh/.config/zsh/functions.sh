73717e10f8be286751ef302867fe350d04b3618c
Ryan Carney <arecarn@gmail.com>
2017-12-20 14:40:10 -0800

    zsh: add functions testbeep and start-ssh-agent

--- zsh/.config/zsh/functions.sh
+++ zsh/.config/zsh/functions.sh
@@ -62,3 +62,19 @@ nmodf()
     _nmod 666 "$@"
 }
 
+# beep once if the command succeeds and twice if it fails
+# usage "some-comand; testbeep
+testbeep() {
+    if [  $? -eq 0 ]; then
+        echo -e '\a'
+    else
+        echo -e '\a'
+        sleep 0.5
+        echo -e '\a'
+    fi
+}
+
+start-ssh-agent() {
+    eval $(ssh-agent -s)
+    ssh-add ~/.ssh/id_rsa
+}

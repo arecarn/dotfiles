fd9a4b52c97f54c385eed9b07fc23e0fa231b128
Ryan Carney <arecarn@gmail.com>
2018-01-02 13:44:25 -0800

    git: add pre-commit hook to check for up to date copyright

---
+++ git/bin/pre_commit_copyright_check.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#
+# Check if copyright statements include the current year
+#
+
+# Check if this is the initial commit
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+    against=HEAD
+else
+    # Initial commit: diff against an empty tree object
+    against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+file_list=$(git diff --cached --name-only "${against}")
+year=$(date +"%Y")
+
+
+for file in ${file_list}; do
+    grep -i copyright >/dev/null 2>&1 || continue
+
+    if ! grep -i -e "copyright.*${year}" "${file}" >/dev/null 2>&1; then
+        missing_copyright_files="${missing_copyright_files} ${file}"
+    fi
+done
+
+if [ -n "${missing_copyright_files}" ]; then
+    echo "pre-commit: ${year} is missing in the copyright notice of the following files:"
+    for file in ${missing_copyright_files}; do
+        printf '    %s\n' "${file}"
+    done
+    exit 1
+fi

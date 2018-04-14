24d2b57bc05847e97683de19d4d957dcdd900478
Ryan Carney <arecarn@gmail.com>
2018-04-14 12:54:56 -0700

    git: fix unquoted variable in pre-push hook

--- git/bin/pre_push_check_for_valid_branch_name.sh
+++ git/bin/pre_push_check_for_valid_branch_name.sh
@@ -19,7 +19,7 @@
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 valid_chars="[a-z0-9_-]"
 
-if ! echo $local_branch | grep -q "^${valid_chars}\+$"; then
+if ! echo "$local_branch" | grep -q "^${valid_chars}\+$"; then
     echo "pre-push: Aborting push due to invalid branch name \"$local_branch\". Use the following characters $valid_chars instead"
     exit 1
 fi

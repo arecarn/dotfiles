16d9c7ebc229fd97a779f836f96b47d41fe98103
Ryan Carney <arecarn@gmail.com>
2020-07-21 15:22:43 -0700

    powershell: add history search using ctrl-p and ctrl-n

--- powershell/Documents/WindowsPowerShell/profile.ps1
+++ powershell/Documents/WindowsPowerShell/profile.ps1
@@ -1,15 +1,20 @@
-# Also take into account the following problem. You may have the following error while executing the file located in $profile:
-#     "Microsoft.PowerShell_profile.ps" cannot be loaded because the execution of scripts is disabled on this system. Please see "get-help about_signing" for more details.
+# Also take into account the following problem. You may have the following error
+# while executing the file located in $profile:
+#     "Microsoft.PowerShell_profile.ps" cannot be loaded because the execution
+#     of scripts is disabled on this system. Please see "get-help about_signing"
+#     for more details.
 #
 # Solution: Check the current execution-policy
 #     PS C:\Windows\System32> Get-ExecutionPolicy
 #     Restricted
 #
-# To change the execution policy to allow PowerShell to execute scripts from local files, run the following command:
+# To change the execution policy to allow PowerShell to execute scripts from
+# local files, run the following command:
 #     PS C:\Windows\System32> Set-Executionpolicy RemoteSigned -Scope CurrentUser
-#
 
 Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Chord 'Ctrl+p' -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord 'Ctrl+n' -Function HistorySearchForward
 
 set-alias g git
 set-alias e nvim-qt.exe

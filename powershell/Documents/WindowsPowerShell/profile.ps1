a9d3c5e3fda02042bf1df489af0c51aa280e5c79
Ryan Carney <arecarn@gmail.com>
2020-07-31 13:57:57 -0700

    powershell: setup X forwarding and fzf

--- powershell/Documents/WindowsPowerShell/profile.ps1
+++ powershell/Documents/WindowsPowerShell/profile.ps1
@@ -11,10 +11,28 @@
 # To change the execution policy to allow PowerShell to execute scripts from
 # local files, run the following command:
 #     PS C:\Windows\System32> Set-Executionpolicy RemoteSigned -Scope CurrentUser
+#
+
+function install_ps_modules
+{
+    # TODO functions to update modules
+    Install-Module PSFzf -Scope CurrentUser
+}
 
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord 'Ctrl+p' -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Chord 'Ctrl+n' -Function HistorySearchForward
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+Remove-PSReadlineKeyHandler 'Ctrl+t'
+Import-Module PSFzf
+Enable-PsFzfAliases
+
+
+# add a display for use with ssh
+$env:DISPLAY="127.0.0.1:0"
+
+# Convenience function to call ssh with X forwarding enabled
+function sshx { ssh -Y $args }
 
 set-alias g git
 set-alias e nvim-qt.exe

612884dba01ff2afd1d1375a65316eef87fd358a
Ryan Carney <arecarn@gmail.com>
2023-01-03 12:49:39 -0800

    git: make branch name check more permissive

--- git/bin/pre_commit_ascii_filename_check.sh
+++ git/bin/pre_commit_ascii_filename_check.sh
@@ -14,7 +14,7 @@ else
 fi
 
 # If you want to allow non-ASCII filenames set this variable to true.
-allownonascii=$(git config hooks.allownonascii)
+allow_non_ascii=$(git config hooks.allownonascii)
 
 # Redirect output to stderr.
 exec 1>&2
@@ -22,7 +22,7 @@ exec 1>&2
 # Cross platform projects tend to avoid non-ASCII filenames; prevent
 # them from being added to the repository. We exploit the fact that the
 # printable range starts at the space character and ends with tilde.
-if [ "$allownonascii" != "true" ]; then
+if [ "$allow_non_ascii" != "true" ]; then
     file_list=$(git diff --cached --name-only --diff-filter=A -z "${against}")
     # Note that the use of brackets around a tr range is ok here, (it's even
     # required, for portability to Solaris 10's /usr/bin/tr), since the square

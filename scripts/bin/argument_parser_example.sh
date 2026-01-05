1986479b4f325ebf0cfd7105185e55c00d408d73
Ryan Carney <arecarn@gmail.com>
2026-01-05 10:34:14 -0800

    argument_parser_example: extend to have required arguments

--- scripts/bin/argument_parser_example.sh
+++ scripts/bin/argument_parser_example.sh
@@ -2,6 +2,31 @@
 
 # based on: http://stackoverflow.com/a/31443098/2228183
 
+# Function to handle required arguments
+handle_required_argument() {
+    var_name="$1"
+    value="$2"
+    arg_name="${3}"
+
+    if [ -z "$value" ]; then
+        echo "Error: $arg_name requires a value" >&2
+        exit 1
+    fi
+
+    eval "$var_name='$value'"
+}
+
+# Function to check if a required argument is set
+check_required_argument() {
+    var_name="$1"
+    arg_name="${2:-$1}"
+
+    if [ -z "$(eval echo \\$$var_name)" ]; then
+        echo "Error: $arg_name is required" >&2
+        exit 1
+    fi
+}
+
 positional_arguments=''
 collect_positional_argument(){
     # save all the non-named arguments so they can be reinserted back into the
@@ -19,15 +44,15 @@ while [ "$#" -gt 0 ]; do
     case "$1" in
 
         -n|--name)
-            name="$2";
+            handle_required_argument name "$2" "$1"
             shift 1;;
 
         -f|--file)
-            file="$2";
+            handle_required_argument file "$2" "$1"
             shift 1;;
 
         -l|--log)
-            log="$2";
+            handle_required_argument log "$2" "$1"
             shift 1;;
 
         -d|--default)
@@ -42,6 +67,11 @@ while [ "$#" -gt 0 ]; do
     esac
     shift 1
 done
+
+# Check for required arguments
+check_required_argument name "-n/--name"
+check_required_argument file "-f/--file"
+check_required_argument log "-l/--log"
 
 # reinsert non-named arguments back into positional parameters ($1 $2 ..)
 eval set -- "${positional_arguments}"

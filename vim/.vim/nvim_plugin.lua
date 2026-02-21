cb4c916312045a23cb8b3266350cbcc91d288838
Ryan Carney <arecarn@gmail.com>
2026-02-20 16:11:10 -0800

    vim: integrate Overseer task runner and revamp theme setup
    - Integrate overseer.nvim: replace vim-dispatch, add setup_overseer, Make command, keymaps and completion for Makefile targets
    - Migrate Apprentice theme to use lush.nvim and adisen99/apprentice.nvim with Lua-based configuration
    - add picker config in snacks.nvim setup

--- vim/.vim/nvim_plugin.lua
+++ vim/.vim/nvim_plugin.lua
@@ -6,11 +6,37 @@ function setup_nvim_snacks()
     config = {
         input = { enabled = true },
         indent = { enabled = true },
+        picker = {},
     }
     snacks.setup(config)
 end
 
+function setup_overseer()
+    require("overseer").setup()
+
+    vim.api.nvim_create_user_command("Make", function(params)
+      -- Insert args at the '$*' in the makeprg
+      local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
+      if num_subs == 0 then
+        cmd = cmd .. " " .. params.args
+      end
+      local task = require("overseer").new_task({
+        cmd = vim.fn.expandcmd(cmd),
+        components = {
+          { "on_output_quickfix", open = not params.bang, open_height = 8 },
+          "default",
+        },
+      })
+      task:start()
+    end, {
+      desc = "Run your makeprg as an Overseer task",
+      nargs = "*",
+      bang = true,
+    })
+end
+
 function setup_nvim_cmp()
+
     local cmp = require('cmp')
 
     cmp.setup{

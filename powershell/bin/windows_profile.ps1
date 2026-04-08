3841e577fc1e60f1190a5414edc4350e784df449
Ryan Carney <arecarn@gmail.com>
2026-04-07 22:39:02 -0700

    powershell: remove manual oh-my-posh path setup
    Chocolatey manages oh-my-posh path and themes at the Machine level.

--- powershell/bin/windows_profile.ps1
+++ powershell/bin/windows_profile.ps1
@@ -1,4 +1,4 @@
 # Set environmental variables
 [Environment]::SetEnvironmentVariable("EDITOR", "nvim", "User")
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$HOME\bin", "User")
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$HOME\AppData\Local\Programs\oh-my-posh\bin", "User")
+# oh-my-posh path and themes are managed by Chocolatey at the Machine level
